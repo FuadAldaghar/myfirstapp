88e7c0d34d7d53d2ae4eb045906e295b217fdcaa
Dev Fuad aldaghar <fuadaldagharaldaghar@gmail.com>
2026-08-20 22:52:03 +0300

    feat: implement ProjectsController CRUD operations and corresponding views for project management

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyFirstApp.Data;
 using MyFirstApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 namespace MyFirstApp.Controllers
 {
     public class ProjectsController : Controller
@@ -25,35 +26,148 @@ namespace MyFirstApp.Controllers
             return View(projects);
         }
 
-        // GET: /Projects/Details/5
+     
         public IActionResult Details(int id)
         {
-             var projects = _context.Projects
-                .Include(p => p.Category)
-                .Include(p => p.Technologies)
-                .Include(p => p.ProjectDetails)
-                .ToList();
 
-            return View(projects);
+            //   return  View("Index");
+                var projects = _context.Projects.Where(p=>p.Id==id)
+                 .Include(p => p.Category)
+                 .Include(p => p.Technologies)
+                 .Include(p => p.ProjectDetails).ToList();
+               
+
+            return View(projects[0]);
           
         }
 
-        // GET: /Projects/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+     [HttpGet]
+public IActionResult Create()
+{
+    ViewBag.Categories = new SelectList(
+        _context.Categories,
+        "Id",
+        "Name"
+    );
+    ViewBag.Technologies = new SelectList(
+        _context.Technologies,
+        "Id",
+        "Name"
+    );
+
+
+    return View();
+}
+        [HttpPost]
+[ValidateAntiForgeryToken]
+public IActionResult Create(Project project, int[] technologyIds)
+{
+
+
+    if (!ModelState.IsValid)
+    {
+        return View(project);
+    }
+
+    project.ProjectDetails ??= new ProjectDetails();
+
+    var technologies = _context.Technologies
+        .Where(t => technologyIds.Contains(t.Id))
+        .ToList();
+
+    project.Technologies = technologies;
+
+    _context.Projects.Add(project);
+
+    _context.SaveChanges();
+
+    return RedirectToAction(nameof(Index));
+}
+
+
 
         // GET: /Projects/Edit/5
         public IActionResult Edit(int id)
         {
-            return View();
-        }
+               var project=_context.Projects
+               .Include(p => p.Category)
+                .Include(p => p.Technologies)
+                .Include(p => p.ProjectDetails)
+                .Where(p => p.Id == id)
+                .FirstOrDefault();
 
-        // GET: /Projects/Delete/5
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name");
+            ViewBag.Technologies = new SelectList(_context.Technologies, "Id", "Name");
+
+            return View(project);
+        }
+        [HttpPost]
+[ValidateAntiForgeryToken]
+public IActionResult Edit(Project project, int[] technologyIds)
+{
+    if (!ModelState.IsValid)
+    {
+        ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name");
+        ViewBag.Technologies = new SelectList(_context.Technologies, "Id", "Name");
+        return View(project);
+    }
+
+    var existingProject = _context.Projects
+        .Include(p => p.Technologies)
+        .Include(p => p.ProjectDetails)
+        .FirstOrDefault(p => p.Id == project.Id);
+
+    if (existingProject == null)
+    {
+        return NotFound();
+    }
+
+    // Update simple properties
+    existingProject.Name = project.Name;
+    existingProject.Description = project.Description;
+    existingProject.GitHubUrl = project.GitHubUrl;
+    existingProject.CategoryId = project.CategoryId;
+    existingProject.IsPublic = project.IsPublic;
+    existingProject.ProjectType = project.ProjectType;
+    existingProject.License = project.License;
+    existingProject.TechnologiesCount = project.TechnologiesCount;
+
+    // Update ProjectDetails
+    existingProject.ProjectDetails ??= new ProjectDetails();
+    existingProject.ProjectDetails.Client = project.ProjectDetails.Client;
+    existingProject.ProjectDetails.StartDate = project.ProjectDetails.StartDate;
+    existingProject.ProjectDetails.EndDate = project.ProjectDetails.EndDate;
+    existingProject.ProjectDetails.Budget = project.ProjectDetails.Budget;
+
+    // Update technologies (many-to-many)
+    existingProject.Technologies.Clear();
+    var technologies = _context.Technologies
+        .Where(t => technologyIds.Contains(t.Id))
+        .ToList();
+
+    existingProject.Technologies = technologies;
+
+    _context.SaveChanges();
+
+    return RedirectToAction(nameof(Index));
+}
+
+        [HttpPost]
         public IActionResult Delete(int id)
-        {
-            return View();
+        { 
+        //    _context.Projects.Where(p =>p.Id==id).ExecuteDelete();
+        var project=_context.Projects.Find(id);
+        if(project!=null)
+        {_context.Projects.Remove(project);
+            _context.SaveChanges();
+        }
+            
+            return RedirectToAction("Index");
         }
     }
 }
